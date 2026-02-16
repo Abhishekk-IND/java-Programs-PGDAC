d1c554885df84813c62d20dc1bc1647e83fef635
abhishekshahab <abhishekdasondhi1842003@gmailcom>
2026-02-16 14:52:06 +0530

    TwentyFour programm added

--- src/JavaPrograms/TwentyFour.java
+++ src/JavaPrograms/TwentyFour.java
@@ -4,7 +4,7 @@ import org.w3c.dom.ls.LSOutput;
 public class TwentyFour {
 
     public static void main(String[] args){
-        System.out.println("welcome to print right half pyramid");
+        System.out.println("welcome to print right half pyramid with *");
         for (int i =1; i<=5; i++){
             for (int j =1 ;j <=i; j++){
                 System.out.print("* ");
